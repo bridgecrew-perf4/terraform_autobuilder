4b61cc4c48de86a4716a51496f128b52411e2827
Michael Verhulst <michael@terminallabs.com>
2020-11-13 11:33:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,8 +25,5 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "my-test-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
-
-  tags {
-    Name = "test-instance"
-  }
+  tags            = {Name="test-instance"}
 }
